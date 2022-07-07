a56437f6568513e256b212e91a35d5a305d711fe
Yu Xiang <99579760+fedrium@users.noreply.github.com>
2022-07-07 18:05:53 +0800

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cyu-xian <cyu-xian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 13:52:39 by cyu-xian          #+#    #+#             */
-/*   Updated: 2022/06/27 15:38:10 by cyu-xian         ###   ########.fr       */
+/*   Updated: 2022/06/29 14:26:34 by cyu-xian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,20 @@ char	*get_next_line(int fd)
 {
 	int	i;
 	static	char	*buffer;
+	char	*retv;
 	buffer = (char*)sizeof(char) * BUFFER_SIZE;
 
 	while (buffer[i] != '\0')
 	{
 		read(fd, buffer, BUFFER_SIZE);
 		i = 0;
-		while (buffer[i] != '\n')
+		while (buffer[i] <= BUFFER_SIZE)
 		{
-			
+			if (i == BUFFER_SIZE || buffer[i] != '\n')
+				ft_strjoin(retv, buffer);
+			if (buffer[i] == '\n')
+
+			i++;
 		}
 	}
 }
